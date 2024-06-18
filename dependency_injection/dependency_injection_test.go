67d8d6df95d06b5c88e8d173544d6bfb76a46375
Samuel Araújo Venâncio <contatos.samuel@outlook.com>
2024-06-18 15:30:59 -0300

    Dependency injection section done

---
+++ dependency_injection/dependency_injection_test.go
@@ -0,0 +1,18 @@
+package dependencyinjection
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Samuel")
+
+	result := buffer.String()
+	expected := "Hello, Samuel"
+
+	if result != expected {
+		t.Errorf("result '%s', expected '%s'", result, expected)
+	}
+}
