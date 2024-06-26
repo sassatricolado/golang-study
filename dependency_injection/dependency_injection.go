07e59ee531aec872341e68cf2be24357fe45e87c
Samuel Araújo Venâncio <contatos.samuel@outlook.com>
2024-06-26 10:24:55 -0300

    Dependency injection section finished

--- dependency_injection/dependency_injection.go
+++ dependency_injection/dependency_injection.go
@@ -1,10 +1,23 @@
-package dependencyinjection
+package main
 
 import (
 	"fmt"
 	"io"
+	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func HandleGreet(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	err := http.ListenAndServe(":5000", http.HandlerFunc(HandleGreet))
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
